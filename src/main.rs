a63ea6396022e4b33d9028289d4157d1e64d915e
Logan VanWay <Lvanway@outlook.com>
2023-11-17 21:53:11 -0500

    Specify backend for renderer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::{RenderPlugin, settings::{RenderCreation, Backends, WgpuSettings}}};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(RenderPlugin {
+            render_creation: RenderCreation::Automatic(WgpuSettings{backends: Some(Backends::VULKAN), ..default()})
+        }))
         .run();
 }
