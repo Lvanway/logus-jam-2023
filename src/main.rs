5410167576239a6d513b49a67ca321a4e229dfff
Logan VanWay <Lvanway@outlook.com>
2023-11-17 22:20:15 -0500

    MORE

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::default;
+
 use bevy::{prelude::*, render::{RenderPlugin, settings::{RenderCreation, Backends, WgpuSettings}}};
 
 fn main() {
@@ -5,5 +7,37 @@ fn main() {
         .add_plugins(DefaultPlugins.set(RenderPlugin {
             render_creation: RenderCreation::Automatic(WgpuSettings{backends: Some(Backends::VULKAN), ..default()})
         }))
+        .add_state::<GameState>()
+        .add_systems(OnEnter(GameState::MainMenu), setup)
         .run();
+}
+
+
+#[derive(Component)]
+struct Player {
+    position: Vec2,
+    velocity: Vec2,
+    hit_points: i32
+}
+
+impl Player {
+    fn new(position: Vec2, velocity: Vec2, hit_points: i32) -> Self {
+        Self {
+            position, 
+            velocity, 
+            hit_points
+        }
+    }
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+  }
+
+
+#[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+enum GameState {
+    #[default]
+    MainMenu,
+    InGame
 }
